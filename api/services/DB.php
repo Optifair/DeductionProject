2c7a971822567b915e0e41cfba403e308a0f6528
Optifair <optifairpraym@gmail.com>
2022-10-18 17:46:17 +0700

    Link project with database. Create class to interact with the database. Create post controller

---
+++ api/services/DB.php
@@ -0,0 +1,26 @@
+<?php
+namespace services;
+
+use mysqli;
+
+
+class DB 
+{
+    public $db_host = 'localhost';
+    public $db_user = 'test';
+    public $db_password = 'пароль';
+    public $db_database = 'deductionproject';
+
+    public function database()
+    {
+        $conn = new mysqli($this->db_host, $this->db_user, $this->db_password,
+            $this->db_database);
+
+        if($conn->connect_error)
+        {
+            die("Connection failed ".$conn->connect_error);
+        }
+
+        return $conn;
+    }
+}
