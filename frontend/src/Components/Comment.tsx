7994940a6d4d5b5f92d3c2056e9fbfe24ff38e67
Optifair <optifairpraym@gmail.com>
2022-10-27 22:12:10 +0700

    Us - 8 - Comment.tsx - create comment card.

---
+++ frontend/src/Components/Comment.tsx
@@ -0,0 +1,40 @@
+import { Avatar, Card, CardContent, CardHeader, Divider, Typography } from '@mui/material';
+import { red } from '@mui/material/colors';
+
+type Params = {
+    id: string;
+    content: string;
+    userId: string;
+}
+
+
+export default function PostCard ( { id, content, userId }: Params )
+{
+    return (
+        <Card>
+            <CardHeader sx={ { paddingBottom: '0px', fontSize: 'large' } }
+                avatar={
+                    <Avatar sx={ { bgcolor: red[ 500 ], height: 45, width: 45 } } src='https://bit.ly/broken-link' >
+                    </Avatar>
+                }
+                title={
+                    <Typography variant="h6">
+                        { userId }
+                    </Typography>
+                }
+            />
+            <CardContent sx={ { paddingTop: '10px' } }>
+                <Typography variant="body1">
+                    { content }
+                </Typography>
+            </CardContent>
+            <Divider />
+        </Card>
+    );
+}
+
+
+
+
+
+
