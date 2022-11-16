1c5fefbbc2fb5eb2f9c971051ced0b492e11e88e
Optifair <optifairpraym@gmail.com>
2022-11-16 18:53:32 +0700

    US-7 - Api - Add iteractions with marks in post controller and repository.

--- Api/Controllers/PostsController.php
+++ Api/Controllers/PostsController.php
@@ -2,16 +2,14 @@
 
 namespace Api\Controllers;
 
-use Api\Services\DB;
 use Api\Models\Repositories\PostRepository;
-use Api\Controllers\Controller;
+use Api\Models\Repositories\UserRepository;
 
 class PostsController extends Controller
 {
     public function getPostsFromDatabase()
     {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: *");
+        self::setCORSHeaders();
 
         $perPage = $_GET['limit'] ?? 5;
         $pageNumber = $_GET['offset'] ?? 0;
@@ -21,30 +19,62 @@ class PostsController extends Controller
         echo json_encode($postsArray, JSON_PRETTY_PRINT);
     }
 
-    public function searchPostsByKey()
+    public function searchPosts()
     {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: *");
+        self::setCORSHeaders();
 
         $perPage = $_GET['limit'] ?? 5;
         $pageNumber = $_GET['offset'] ?? 0;
 
         $key = $_GET['key'] ?? '';
+        $startDate = $_GET['startDate'] ?? '2014-08-18T21:11:54';
+        $endDate = $_GET['endDate'] ?? '3014-08-18T21:11:54';
 
-        $postsArray['posts'] = PostRepository::searchPostsByKey($perPage, $pageNumber, $key);
+        $postsArray['posts'] = PostRepository::searchPosts($perPage, $pageNumber, $key, $startDate, $endDate);
 
         echo json_encode($postsArray, JSON_PRETTY_PRINT);
     }
 
     public function getCurrentPost()
     {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: *");
+        self::setCORSHeaders();
 
-        self::getHeaders();
         $id = $_GET['id'] ?? null;
 
         $postsArray['posts'] = PostRepository::getCurrentPost($id);
+
+        echo json_encode($postsArray, JSON_PRETTY_PRINT);
+    }
+
+    public function MarkPost()
+    {
+        self::setCORSHeaders();
+
+        $json = file_get_contents('php://input');
+        $data = json_decode($json);
+
+        $post_id = $data->id;
+        $login = $_COOKIE['login'];
+        $user = UserRepository::findUserByLogin($login);
+
+        $mark = PostRepository::findMark($user['id'], $post_id);
+        if (!empty($mark)) {
+            PostRepository::deleteMark($mark[0]['mark_id']);
+        } else {
+            PostRepository::addMark($user['id'], $post_id);
+        }
+    }
+
+    public function getMarks()
+    {
+        self::setCORSHeaders();
+
+        $login = $_COOKIE['login'];
+
+        $perPage = $_GET['limit'] ?? 5;
+        $pageNumber = $_GET['offset'] ?? 0;
+
+        $postsArray['posts'] = PostRepository::getMarks($login, $perPage, $pageNumber);
 
         echo json_encode($postsArray, JSON_PRETTY_PRINT);
     }
--- Api/Models/Repositories/PostRepository.php
+++ Api/Models/Repositories/PostRepository.php
@@ -2,8 +2,6 @@
 
 namespace Api\Models\Repositories;
 
-use Api\Models\Repositories\Repository;
-use Exception;
 use Api\Models\Tools\QueryObject as QO;
 
 class PostRepository extends Repository
@@ -17,9 +15,28 @@ class PostRepository extends Repository
             'user_id',
             'title',
             'content',
-            'image'
+            'image',
+            'date'
         );
 
+        $posts = self::executeQuery($query);
+        return $posts;
+    }
+
+    public static function getMarks($login, $perPage, $pageNumber): array
+    {
+        self::prepareExecution();
+        $query = QO::select()->table('posts')->join(['marks', 'posts'], ['post_id', 'id'])->
+        join(['users', 'marks'], ['id', 'mark_user_id'])->where(['login', $login, '='])->limit($perPage)->offset($pageNumber);
+        $query->columns(
+            'posts.id',
+            'user_id',
+            'title',
+            'content',
+            'image',
+            'date',
+            'mark_date'
+        );
         $posts = self::executeQuery($query);
         return $posts;
     }
@@ -27,32 +44,69 @@ class PostRepository extends Repository
     public static function getCurrentPost($id): array
     {
         self::prepareExecution();
-        $query = QO::select()->table('posts')->where(['id',$id]);
+        $query = QO::select()->table('posts')->where(['id', $id, '=']);
         $query->columns(
             'id',
             'user_id',
             'title',
             'content',
-            'image'
+            'image',
+            'date'
         );
 
         $post = self::executeQuery($query)[0];
         return $post;
     }
 
-    public static function searchPostsByKey($perPage, $pageNumber, $key): array
+    public static function searchPosts($perPage, $pageNumber, $key, $startDate, $endDate): array
     {
         self::prepareExecution();
-        $query = QO::select()->table('posts')->limit($perPage)->offset($pageNumber)->like($key);
+        $query = QO::select()->table('posts')->limit($perPage)->offset($pageNumber)->
+        where(['date', $startDate, '>='])->where(['date', $endDate, '<='])->like($key);
         $query->columns(
             'id',
             'user_id',
             'title',
             'content',
-            'image'
+            'image',
+            'date'
         );
-
         $posts = self::executeQuery($query);
         return $posts;
+    }
+
+    public static function addMark($user_id, $post_id)
+    {
+        self::prepareExecution();
+        $query = QO::insert()->table('marks')->columns('mark_user_id', 'post_id');
+
+        $query->values(
+            $user_id,
+            $post_id,
+        );
+        self::executeQuery($query, false);
+    }
+
+    public static function findMark($user_id, $post_id)
+    {
+        self::prepareExecution();
+        $query = QO::select()->table('marks')->where(['mark_user_id', $user_id, '='])
+            ->where(['post_id', $post_id, '=']);
+        $query->columns(
+            'mark_id',
+            'mark_user_id',
+            'post_id'
+        );
+        $mark = self::executeQuery($query);
+        return $mark;
+
+    }
+
+    public static function deleteMark($mark_id)
+    {
+        self::prepareExecution();
+        $query = QO::delete()->table('marks')->where(['mark_id', $mark_id, '=']);
+        echo $query;
+        $mark = self::executeQuery($query)[0];
     }
 }
