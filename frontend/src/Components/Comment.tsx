2007b47d880c6554198f3131474dabcda294b838
Optifair <optifairpraym@gmail.com>
2022-11-16 18:39:23 +0700

    US-5 - frontend - Style files.

--- frontend/src/Components/Comment.tsx
+++ frontend/src/Components/Comment.tsx
@@ -1,34 +1,34 @@
-import { Avatar, Card, CardContent, CardHeader, Divider, Typography } from '@mui/material';
-import { red } from '@mui/material/colors';
+import {Avatar, Card, CardContent, CardHeader, Divider, Typography} from '@mui/material';
+import {red} from '@mui/material/colors';
 
 type Params = {
     id: string;
     content: string;
     userId: string;
+    date: string;
 }
 
 
-export default function PostCard ( { id, content, userId }: Params )
-{
+export default function PostCard({id, content, userId, date}: Params) {
     return (
         <Card>
-            <CardHeader sx={ { paddingBottom: '0px', fontSize: 'large' } }
-                avatar={
-                    <Avatar sx={ { bgcolor: red[ 500 ], height: 45, width: 45 } } src='https://bit.ly/broken-link' >
-                    </Avatar>
-                }
-                title={
-                    <Typography variant="h6">
-                        { userId }
-                    </Typography>
-                }
+            <CardHeader sx={{paddingBottom: '0px', fontSize: 'large'}}
+                        avatar={
+                            <Avatar sx={{bgcolor: red[500], height: 45, width: 45}} src='https://bit.ly/broken-link'>
+                            </Avatar>
+                        }
+                        title={
+                            <Typography variant="h6">
+                                {userId}
+                            </Typography>
+                        }
             />
-            <CardContent sx={ { paddingTop: '10px' } }>
+            <CardContent sx={{paddingTop: '10px'}}>
                 <Typography variant="body1">
-                    { content }
+                    {content}
                 </Typography>
             </CardContent>
-            <Divider />
+            <Divider/>
         </Card>
     );
 }
--- frontend/src/Components/PostsListPage.tsx
+++ frontend/src/Components/PostsListPage.tsx
@@ -1,55 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import { Stack } from '@mui/system';
+import React, {useEffect, useState} from 'react';
+import {Stack} from '@mui/system';
 import PostCard from './PostCard';
-import { Box, Button } from '@mui/material';
+import {Button} from '@mui/material';
 import ScrollTopButton from './ScrollTopButton';
 import BackAdress from '../BackAdress';
 
-export default function PostsListPage ()
-{
-  const [ postsTotal, setPostsTotal ] = useState( undefined );
-  const [ posts, setPosts ] = useState( [] );
-  const [ pageSize, setPageSize ] = useState<number>( 5 );
+export default function PostsListPage() {
+    const [postsTotal, setPostsTotal] = useState(undefined);
+    const [posts, setPosts] = useState([]);
+    const [pageSize, setPageSize] = useState<number>(5);
 
 
-  const offset = 0;
+    const offset = 0;
 
-  async function fetchPosts ( pageSize: any, offset: any )
-  {
-    const res = await fetch(
-      `http://${ BackAdress }/api/posts?limit=${ pageSize }&offset=${ offset }`
+    async function fetchPosts(pageSize: any, offset: any) {
+        const res = await fetch(
+            `http://${BackAdress}/api/posts?limit=${pageSize}&offset=${offset}`
+        );
+
+        return await res.json();
+    }
+
+    useEffect(() => {
+        fetchPosts(pageSize, offset).then((posts) => {
+            setPostsTotal(posts.count);
+            setPosts(posts.posts);
+            console.log(posts);
+        });
+    }, [pageSize, offset])
+
+    async function loadNewPosts() {
+        setPageSize((pageSize) => pageSize + 5);
+    }
+
+
+    return (
+        <Stack spacing={3} alignItems={'center'} paddingBottom={'30px'}>
+            <Stack spacing={4} className="s" alignItems={'center'} paddingTop={'90px'}>
+                {posts.map(function ({id, title, content, user_id, image, date}) {
+                    return <PostCard key={id} id={id} title={title} content={content} userId={user_id}
+                                     image={image} date={date}/>
+                })}
+            </Stack>
+            <Button onClick={loadNewPosts} variant="outlined"
+                    style={{border: '1px solid ghostwhite', color: 'ghostwhite'}}>Show more</Button>
+            <ScrollTopButton/>
+        </Stack>
     );
-
-    return await res.json();
-  }
-
-  useEffect( () =>
-  {
-    fetchPosts( pageSize, offset ).then( ( posts ) =>
-    {
-      setPostsTotal( posts.count );
-      setPosts( posts.posts );
-    } );
-  }, [ pageSize, offset ] )
-
-  async function loadNewPosts ()
-  {
-    setPageSize( ( pageSize ) => pageSize + 5 );
-  }
-
-
-  return (
-    <Stack spacing={ 3 } alignItems={ 'center' } paddingBottom={ '30px' }>
-      <Stack spacing={ 4 } className="s" alignItems={ 'center' } paddingTop={ '90px' }>
-        { posts.map( function ( { id, title, content, user_id, image } )
-        {
-          return <PostCard key={ id } id={ id } title={ title } content={ content } userId={ user_id }
-            image={ image } />
-        } ) }
-      </Stack>
-      <Button onClick={ loadNewPosts } variant="outlined" style={ { border: '1px solid ghostwhite', color: 'ghostwhite' } }>Show more</Button>
-      <ScrollTopButton />
-    </Stack>
-  );
 
 }
