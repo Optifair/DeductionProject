9e512e4c97356fd7bce25bd67a9c21f8c46f5aa7
Optifair <optifairpraym@gmail.com>
2022-11-16 18:33:57 +0700

    US-5 - Api - refactor Headers method. Add CORS support.

--- Api/Controllers/Controller.php
+++ Api/Controllers/Controller.php
@@ -2,28 +2,13 @@
 
 namespace Api\Controllers;
 
-use Api\Services\DB;
-
 class Controller
 {
-    public static function getHeaders()
+    public static function setCORSHeaders()
     {
-        header("Access-Control-Allow-Origin: *");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');
-        header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
-
-
-        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-            }
-
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-            }
-
-            exit(0);
-        }
+        header("Access-Control-Allow-Origin: http://deductionproject:3000");
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Headers: access-control-allow-credentials, access-control-allow-origin, content-type");
+        header("Access-Control-Allow-Methods: *");
     }
 }
