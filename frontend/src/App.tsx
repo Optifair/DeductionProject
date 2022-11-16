ac5159e4f15cb2a17643bef982153e6e7fc8ac30
Optifair <optifairpraym@gmail.com>
2022-11-16 18:35:57 +0700

    US-5 - frontend - Add new frontend routing paths.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,47 @@
 import Header from './Components/Header';
-import
-{
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import PostsListPage from './Components/PostsListPage';
-import { Box } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {Box} from '@mui/material';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import AuthPage from './Components/AuthPage';
 import NotFoundPage from './Components/NotFoundPage';
 import SearchPage from './Components/SearchPage';
+import RegistrationPage from "./Components/RegistrationPage";
+import ProfilePage from "./Components/ProfilePage";
+import MarksPage from "./Components/MarksPage";
 
-const darkTheme = createTheme( {
-  palette: {
-    mode: 'dark',
-  },
-} );
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
-export default function App ()
-{
-  const routes = [
-    { path: '/', element: <PostsListPage /> },
-    { path: '/search/:keyWord', element: <SearchPage /> },
-    { path: '/404', element: <NotFoundPage /> },
-  ];
+export default function App() {
+    const routes = [
+        {path: '/', element: <PostsListPage/>},
+        {path: '/search/:keyWord', element: <SearchPage/>},
+        {path: '/auth', element: <AuthPage/>},
+        {path: '/registration', element: <RegistrationPage/>},
+        {path: '/profile', element: <ProfilePage/>},
+        {path: '/marks', element: <MarksPage/>},
+        {path: '/404', element: <NotFoundPage/>},
+    ];
 
-  return (
-    <ThemeProvider theme={ darkTheme }>
-      <CssBaseline />
-      <Box>
-        <Router>
-          <Header />
-          <Routes>
-            { routes.map( ( route, key ) => (
-              <Route path={ route.path } element={ route.element } key={ key } />
-            ) ) }
-          </Routes>
-        </Router>
-      </Box>
-    </ThemeProvider>
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline/>
+            <Box>
+                <Router>
+                    <Header/>
+                    <Routes>
+                        {routes.map((route, key) => (
+                            <Route path={route.path} element={route.element} key={key}/>
+                        ))}
+                    </Routes>
+                </Router>
+            </Box>
+        </ThemeProvider>
 
-  );
+    );
 }
