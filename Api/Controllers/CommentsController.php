07685a5bf4b117b0b1c72c9c56a7466152c9929a
Optifair <optifairpraym@gmail.com>
2022-11-29 12:42:47 +0700

    US-8 - Api - Create addComment function on CommentsController.

--- Api/Controllers/CommentsController.php
+++ Api/Controllers/CommentsController.php
@@ -3,6 +3,7 @@
 namespace Api\Controllers;
 
 use Api\Models\Repositories\CommentRepository;
+use Api\Models\Repositories\UserRepository;
 
 class CommentsController extends Controller
 {
@@ -16,5 +17,25 @@ class CommentsController extends Controller
         $commentsArray['comments'] = CommentRepository::getComments($post_id, $perPage, $pageNumber);
 
         echo json_encode($commentsArray, JSON_PRETTY_PRINT);
+    }
+
+    public function addComment()
+    {
+        self::setCORSHeaders();
+        $authRes = self::checkAuth();
+        if (!empty(file_get_contents('php://input'))) {
+            if ($authRes['auth']) {
+                $json = file_get_contents('php://input');
+                $data = json_decode($json);
+
+                $post_id = $data->post_id;
+                $content = $data->content;
+                $login = $_COOKIE['login'];
+                $user_id = UserRepository::findUserByLogin($login)['id'];
+
+                CommentRepository::addComment($user_id, $post_id, $content);
+            }
+            echo json_encode($authRes, JSON_PRETTY_PRINT);
+        }
     }
 }
