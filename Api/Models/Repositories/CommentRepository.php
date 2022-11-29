c4e4fae44fcfe3a189d3a06551f5504f7cc0f683
Optifair <optifairpraym@gmail.com>
2022-11-29 12:54:23 +0700

    US-5 - Api - Create new methods for CommentRepository and added the ability to load new elements.
    Create methods for add, find, like and dislike comments. getComments method now load like state, avatar and name of user, id of post.

--- Api/Models/Repositories/CommentRepository.php
+++ Api/Models/Repositories/CommentRepository.php
@@ -6,18 +6,68 @@ use Api\Models\Tools\QueryObject as QO;
 
 class CommentRepository extends Repository
 {
-    public static function getComments($user_id, $perPage, $pageNumber): array
+    public static function getComments($post_id, $perPage, $pageNumber): array
     {
         self::prepareExecution();
-        $query = QO::select()->table('comments')->where(['user_id', $user_id, '='])->limit($perPage)->offset($pageNumber);
+        $query = QO::select()->table('comments')->join(['users', 'comments'], ['id', 'user_id'])->where(['post_id', $post_id, '='])->limit($perPage)->offset($pageNumber);
         $query->columns(
-            'id',
+            'comments.id',
             'user_id',
+            'post_id',
             'content',
-            'date'
+            'liked',
+            'date',
+            'avatar',
+            'name'
         );
-
         $comments = self::executeQuery($query);
         return $comments;
+    }
+
+    public static function addComment($user_id, $post_id, $content)
+    {
+        self::prepareExecution();
+        $query = QO::insert()->table('comments')->columns('user_id', 'post_id', 'content');
+
+        $query->values(
+            $user_id,
+            $post_id,
+            $content
+        );
+        self::executeQuery($query, false);
+    }
+
+    public static function findComment($id): array
+    {
+        self::prepareExecution();
+        $query = QO::select()->table('comments')->join(['users', 'comments'], ['id', 'user_id'])->where(['comments.id', $id, '=']);
+        $query->columns(
+            'comments.id',
+            'user_id',
+            'post_id',
+            'content',
+            'liked',
+            'date',
+            'avatar',
+            'name'
+        );
+        $comment = self::executeQuery($query)[0];
+        return $comment;
+    }
+
+    public static function likeComment($comment_id)
+    {
+        self::prepareExecution();
+        $query = QO::update()->table('comments')->columns('liked')->values(1);
+        $query->where(['id', $comment_id, '=']);
+        self::executeQuery($query, false);
+    }
+
+    public static function dislikeComment($comment_id)
+    {
+        self::prepareExecution();
+        $query = QO::update()->table('comments')->columns('liked')->values(0);
+        $query->where(['id', $comment_id, '=']);
+        self::executeQuery($query, false);
     }
 }
