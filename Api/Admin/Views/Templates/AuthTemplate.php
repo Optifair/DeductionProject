ec817856eebbcce08204a1e3fe230f69b0884b06
Optifair <optifairpraym@gmail.com>
2023-01-23 23:20:13 +0700

    US-2 - Admin - Create separate folder for Templates and Auth Template.

---
+++ Api/Admin/Views/Templates/AuthTemplate.php
@@ -0,0 +1,5 @@
+<form action="api/authUser" method="post">
+    <input name="login" placeholder="Логин">
+    <input type="password" name="pass" placeholder="Пароль">
+    <input type="submit" value="Войти">
+</form>
