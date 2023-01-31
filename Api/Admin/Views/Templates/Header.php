904774da2b3baf0f77873ecac7fda93df8fead11
Optifair <optifairpraym@gmail.com>
2023-01-31 12:22:09 +0700

    US-2 - Admin - Create header with tabs to switch between templates.

---
+++ Api/Admin/Views/Templates/Header.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="tab">
+        <a href="/admin/users">
+            <button class="tablinks">Users</button>
+        </a>
+        <a href="/admin/posts">
+            <button class="tablinks">Posts</button>
+        </a>
+        <a href="/admin/comments">
+            <button class="tablinks">Comments</button>
+        </a>
+        <a href="/admin/marks">
+            <button class="tablinks">Marks</button>
+        </a>
+
+        <a href="/admin/logout">
+            <button class="tablinks">Log Out</button>
+        </a>
+    </div>
+</header>
