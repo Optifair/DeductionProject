d632d87538f950d7eee080f61ba5d0a3723b6dbd
Optifair <optifairpraym@gmail.com>
2023-01-31 12:23:44 +0700

    US-2 - Admin - Edit AuthTemplate form route.

--- Api/Admin/Views/Templates/AuthTemplate.php
+++ Api/Admin/Views/Templates/AuthTemplate.php
@@ -1,4 +1,4 @@
-<form action="api/authUser" method="post">
+<form action="/admin/auth" method="post">
     <input name="login" placeholder="Логин">
     <input type="password" name="pass" placeholder="Пароль">
     <input type="submit" value="Войти">
