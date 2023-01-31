f8906b80a8392f1c2e9a72326c8b90722441d629
Optifair <optifairpraym@gmail.com>
2023-01-31 12:06:19 +0700

    US-2 - Controllers - Delete checkAuth method from basic controller class.

--- Api/Controllers/Controller.php
+++ Api/Controllers/Controller.php
@@ -2,8 +2,6 @@
 
 namespace Api\Controllers;
 
-use Api\Models\Repositories\UserRepository;
-
 class Controller
 {
     public const frontPath = "http://deductionproject:3000";
@@ -16,30 +14,5 @@ class Controller
         header("Access-Control-Allow-Methods: *");
     }
 
-    public static function checkAuth()
-    {
-        session_start();
-        if (empty($_SESSION['auth']) or $_SESSION['auth'] == false) {
-            if (!empty($_COOKIE['login']) and !empty($_COOKIE['key'])) {
-                $login = $_COOKIE['login'];
-                $key = $_COOKIE['key'];
 
-                $user = UserRepository::findUserByLogin($login);
-
-                if (!empty($user) and !empty($user['key']) and $user['key'] == $key) {
-                    session_start();
-                    $_SESSION['auth'] = true;
-
-                    $_SESSION['id'] = $user['id'];
-                    $_SESSION['login'] = $user['login'];
-                }
-            }
-        }
-        $auth = false;
-        if (!empty($_SESSION['auth']) and $_SESSION['auth']) {
-            $auth = true;
-        }
-        $ret = ['auth' => $auth];
-        return $ret;
-    }
 }
