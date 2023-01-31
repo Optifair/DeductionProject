9e50a6cb12e69f2ac2af462b16c60ef011e6631e
Optifair <optifairpraym@gmail.com>
2023-01-31 12:21:21 +0700

    US-2 - Admin - Create Admin info view.

---
+++ Api/Admin/Views/Templates/AdminInfo.php
@@ -0,0 +1,9 @@
+<div>
+    <h3>
+        Admin panel info
+    </h3>
+    <h5>
+        There are 5 buttons at the top, the first 4 will take you to the tables of the respective entities. You can
+        create, modify and delete any of them. The fifth button allows you to exit the admin panel.
+    </h5>
+</div>
