7f6a70407b77bba5ec970e0fcbc00a899e332a40
Optifair <optifairpraym@gmail.com>
2023-02-07 01:17:47 +0700

    US-2 - Admin - Add notifications to all entity actions. Change getting ID in deletion methods from Get to Post. Add a method to ban and unban users.

--- Api/Admin/AdminControllers/CrudController.php
+++ Api/Admin/AdminControllers/CrudController.php
@@ -6,20 +6,18 @@ use Api\Models\Repositories\CommentRepository;
 use Api\Models\Repositories\MarkRepository;
 use Api\Models\Repositories\PostRepository;
 use Api\Models\Repositories\UserRepository;
+use Api\Services\MailSender;
 
 class CrudController
 {
-    public function deleteUser()
-    {
-        $id = $_GET['id'];
-
-        UserRepository::deleteUser($id);
-        header('Location: \admin\users');
-    }
-
     public function deletePost()
     {
-        $id = $_GET['id'];
+        $id = $_POST['id'];
+
+        $owner_id = $_POST["user_id"];
+        $message = $_POST["message"];
+        $user = UserRepository::findUserById($owner_id);
+        MailSender::createUserNotificationMail($user['login'], $user['name'], "delete", "post", $message);
 
         PostRepository::deletePost($id);
         header('Location: \admin\posts');
@@ -27,7 +25,12 @@ class CrudController
 
     public function deleteComment()
     {
-        $id = $_GET['id'];
+        $id = $_POST['id'];
+
+        $owner_id = $_POST["user_id"];
+        $message = $_POST["message"];
+        $user = UserRepository::findUserById($owner_id);
+        MailSender::createUserNotificationMail($user['login'], $user['name'], "delete", "comment", $message);
 
         CommentRepository::deleteComment($id);
         header('Location: \admin\comments');
@@ -35,8 +38,11 @@ class CrudController
 
     public function deleteMark()
     {
-        $id = $_GET['id'];
-
+        $id = $_POST['mark_id'];
+        $owner_id = $_POST["user_id"];
+        $message = $_POST["message"];
+        $user = UserRepository::findUserById($owner_id);
+        MailSender::createUserNotificationMail($user['login'], $user['name'], "delete", "mark", $message);
         MarkRepository::deleteMark($id);
         header('Location: \admin\marks');
     }
@@ -44,6 +50,11 @@ class CrudController
     public function editUser()
     {
         $keyLogin = $_POST['keyLogin'];
+
+        $message = $_POST["message"];
+        $user = UserRepository::findUserByLogin($keyLogin);
+        MailSender::createUserNotificationMail($user['login'], $user['name'], "edit", "user data", $message);
+
         UserRepository::updateName($keyLogin, $_POST['name']);
         UserRepository::updateSalt($keyLogin, $_POST['salt']);
         UserRepository::updatePass($keyLogin, $_POST['password']);
@@ -51,7 +62,11 @@ class CrudController
         UserRepository::updateAvatar($keyLogin, $_POST['avatar']);
         UserRepository::updateRating($keyLogin, $_POST['rating']);
         UserRepository::updatePass_reset_code($keyLogin, $_POST['pass_reset_code']);
-        UserRepository::updateIsAdmin($keyLogin, $_POST['isAdmin']);
+        $editor = UserRepository::findUserByLogin($_COOKIE['login']);
+        if ($editor['is_admin']) {
+            UserRepository::updateIs_editor($keyLogin, $_POST['is_editor']);
+            echo 1;
+        }
         UserRepository::updateLogin($keyLogin, $_POST['login']);
 
         header('Location: \admin\users');
@@ -60,6 +75,12 @@ class CrudController
     public function editMark()
     {
         $mark_id = $_POST['mark_id'];
+
+        $owner_id = $_POST["user_id"];
+        $message = $_POST["message"];
+        $user = UserRepository::findUserById($owner_id);
+        MailSender::createUserNotificationMail($user['login'], $user['name'], "edit", "mark", $message);
+
         MarkRepository::updateUser_id($mark_id, $_POST['user_id']);
         MarkRepository::updatePost_id($mark_id, $_POST['post_id']);
         MarkRepository::updateDate($mark_id, $_POST['mark_date']);
@@ -71,19 +92,31 @@ class CrudController
     public function editPost()
     {
         $id = $_POST['id'];
+
+        $owner_id = $_POST["user_id"];
+        $message = $_POST["message"];
+        $user = UserRepository::findUserById($owner_id);
+        MailSender::createUserNotificationMail($user['login'], $user['name'], "edit", "post", $message);
+
         PostRepository::updateUser_id($id, $_POST['user_id']);
         PostRepository::updateTitle($id, $_POST['title']);
         PostRepository::updateContent($id, $_POST['content']);
         PostRepository::updateImage($id, $_POST['image']);
         PostRepository::updateDate($id, $_POST['date']);
 
-        
+
         header('Location: \admin\posts');
     }
 
     public function editComment()
     {
         $id = $_POST['id'];
+
+        $owner_id = $_POST["user_id"];
+        $message = $_POST["message"];
+        $user = UserRepository::findUserById($owner_id);
+        MailSender::createUserNotificationMail($user['login'], $user['name'], "edit", "comment", $message);
+
         CommentRepository::updateUser_id($id, $_POST['user_id']);
         CommentRepository::updatePost_id($id, $_POST['post_id']);
         CommentRepository::updateContent($id, $_POST['content']);
@@ -92,5 +125,41 @@ class CrudController
 
 
         header('Location: \admin\comments');
+    }
+
+    public function unbanUser()
+    {
+        $id = $_POST['id'];
+        $user = UserRepository::findUserById($id);
+        if (!$user['is_admin']) {
+            UserRepository::unbanUser($id);
+        }
+        header('Location: \admin\users');
+    }
+
+    public function banUser()
+    {
+        $id = $_POST['id'];
+        $ban_time = $_POST['ban_time'];
+        $message = $_POST["message"];
+        $user = UserRepository::findUserById($id);
+        if (!$user['is_admin']) {
+            UserRepository::banUser($id, $ban_time);
+            MailSender::createUserNotificationMail($user['login'], $user['name'], "ban", "user on $ban_time hours", $message);
+        }
+        header('Location: \admin\users');
+    }
+
+    public function deleteUser()
+    {
+        $id = $_POST['id'];
+
+        $owner_id = $id;
+        $message = $_POST["message"];
+        $user = UserRepository::findUserById($owner_id);
+        MailSender::createUserNotificationMail($user['login'], $user['name'], "delete", "user", $message);
+
+        UserRepository::deleteUser($id);
+        header('Location: \admin\users');
     }
 }
