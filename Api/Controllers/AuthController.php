d05a44cc80d289aaf8d1e41fda4ea3392ba750ff
Optifair <optifairpraym@gmail.com>
2023-02-07 01:51:40 +0700

    US-2 - Api - Add a ban status check and its return of the user to the authentication methods.

--- Api/Controllers/AuthController.php
+++ Api/Controllers/AuthController.php
@@ -9,25 +9,29 @@ class AuthController extends Controller
     public static function authUser($login, $password)
     {
         $user = UserRepository::findUserByLogin($login);
-        $ret = ['auth' => false];
+        $ret = ['auth' => false, 'ban' => false];
         if (!empty($user)) {
             if (UserRepository::verifyPassword($password, $user['salt'], $user['password'])) {
-                session_start();
-                $ret['auth'] = true;
-                $_SESSION['auth'] = true;
-                $_SESSION['id'] = $user['id'];
-                $_SESSION['login'] = $user['login'];
+                if (strtotime($user['ban_timeout']) <= strtotime(date('Y-m-d H:i:s'))) {
+                    session_start();
+                    $ret['auth'] = true;
+                    $_SESSION['auth'] = true;
+                    $_SESSION['id'] = $user['id'];
+                    $_SESSION['login'] = $user['login'];
 
-                $remember = true;
-                $key = self::generateCookie();
-                $time = time() + 60 * 30;
-                if ($remember) {
-                    $time = time() + 60 * 60 * 24 * 30;
+                    $remember = true;
+                    $key = self::generateCookie();
+                    $time = time() + 60 * 30;
+                    if ($remember) {
+                        $time = time() + 60 * 60 * 24 * 30;
+                    }
+
+                    setcookie('login', $user['login'], $time, '/');
+                    setcookie('key', $key, $time, '/');
+                    UserRepository::updateUserCookie($login, $key);
+                } else {
+                    $ret['ban'] = true;
                 }
-
-                setcookie('login', $user['login'], $time, '/');
-                setcookie('key', $key, $time, '/');
-                UserRepository::updateUserCookie($login, $key);
             }
         }
         return $ret;
@@ -46,13 +50,13 @@ class AuthController extends Controller
     public static function checkAuth()
     {
         session_start();
+        $ban = false;
         if (empty($_SESSION['auth']) or $_SESSION['auth'] == false) {
             if (!empty($_COOKIE['login']) and !empty($_COOKIE['key'])) {
                 $login = $_COOKIE['login'];
                 $key = $_COOKIE['key'];
 
                 $user = UserRepository::findUserByLogin($login);
-
                 if (!empty($user) and !empty($user['key']) and $user['key'] == $key) {
                     session_start();
                     $_SESSION['auth'] = true;
@@ -60,13 +64,16 @@ class AuthController extends Controller
                     $_SESSION['id'] = $user['id'];
                     $_SESSION['login'] = $user['login'];
                 }
+                if (strtotime($user['ban_timeout']) > strtotime(date('Y-m-d H:i:s'))) {
+                    $ban = true;
+                }
             }
         }
         $auth = false;
-        if (!empty($_SESSION['auth']) and $_SESSION['auth']) {
+        if (!empty($_SESSION['auth']) and $_SESSION['auth'] and !$ban) {
             $auth = true;
         }
-        $ret = ['auth' => $auth];
+        $ret = ['auth' => $auth, 'ban' => $ban];
         return $ret;
     }
 
